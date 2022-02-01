de5d656030692802c7803f00237b42d1616d6b45
Austin Atkinson <austina@aatrix.com>
2022-01-31 19:45:37 -0600

    Added adding, deleteing, and rendering months.

--- project.js
+++ project.js
@@ -2,13 +2,13 @@
 
 //#region main classes
 class Month {
-    constructor(month){
-        this.month = month;
+    constructor(name) {
+        this.name = name;
         this.goals = []
     }
 }
-class Goal{
-    constructor(description, addedDate, deadline){
+class Goal {
+    constructor(description, addedDate, deadline) {
         this.description = description;
         this.addedDate = addedDate;
         this.deadline = deadline
@@ -17,8 +17,8 @@ class Goal{
 //#endregion
 
 
-class  GoalTrackingService{
-    static url = ""; //ToDo: find the URL to put the app on
+class GoalTrackingService {
+    static url = "https://crudcrud.com/api/882db0943f7c41d2b1c0494b4c2349b2/months";
 
     static getAllMonthlyGoals() {
         return $.get(this.url);
@@ -55,16 +55,48 @@ class DOMManager {
 
     static render(months) {
         this.months = months;
+        const today = new Date().toLocaleDateString('en-CA'); //this returns the date in yyyy-mm-dd formate required for setting value in input box
         $('#monthly-goals').empty();
         for (const month of months) {
-            $('#monthly-goals').prepend(); //ToDo: Add all the months in Months[] with Goal and Deadline. Maybe add an noneditable textbox with todays date for date added?
-            for (const month of month.goals) {
-                $(`#${house._id}`).find('.card-body').append(); //ToDo: add every goal for the month. with a delete button. If we have time add Edit, Update, Cancele button
+            $('#monthly-goals').prepend(
+                `
+                    <div id="${month._id}" class="card">
+                        <div class="card-header">
+                            <h2 class="h2">${month.name}</h2>
+                            <button class="btn btn-danger" onclick="DOMManager.deleteMonth('${month._id}')">Delete</button>
+                        </div>
+                        <div class="card-body">
+                            <div>
+                                <div class="row">
+                                    <div class="col-sm">Date Added (today)</div>
+                                    <div class="col-sm">Goal Description</div>
+                                    <div class="col-sm">Deadline</div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <input type="date" id="${month._id}-goal-added" class="form-control" value="${today}" disabled>
+                                    </div>
+                                    <div class="col-sm">
+                                        <input type="text" id="${month._id}-goal-description" class="form-control">
+                                    </div>
+                                    <div class="col-sm">
+                                        <input type="date" id="${month._id}-goal-deadline" class="form-control">
+                                    </div>
+                                </div>
+                                <button id="${month._id}-new-room" onclick="DOMManager.addGoal('${month._id}')" class="btn btn-primary form-control">Add</button>
+                            </div>
+                        </div>
+                    </div><br>
+                `
+            );
+            for (const goal of month.goals) {
+                 //ToDo: add every goal for the month. with a delete button. If we have time add Edit, Update, Cancele button
             }
         }
     }
 
     static getAllMonthlyGoals() {
+        GoalTrackingService.getAllMonthlyGoals().then(months => this.render(months))
     }
 
     static addGoal(id) {
@@ -74,10 +106,26 @@ class DOMManager {
     }
 
     static deleteMonth(id) {
+        GoalTrackingService.deleteMonth(id)
+        .then(() => {
+            return GoalTrackingService.getAllMonthlyGoals();
+        })
+        .then((months) => this.render(months));
     }
 
-    static createMonth(month) {
+    static createMonth(name) {
+        GoalTrackingService.createMonth(new Month (name))
+        .then(() => {
+            return GoalTrackingService.getAllMonthlyGoals();
+        })
+        .then((months) => this.render(months));
     }
 }
+
+$('#create-new-month').on('click', () => {
+    const txtNewMonthName = $('#new-month-name');
+    DOMManager.createMonth(txtNewMonthName.val());
+    txtNewMonthName.val('');
+})
 
 DOMManager.getAllMonthlyGoals();
